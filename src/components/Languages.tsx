ac71f1b9562b2a5d120e47eb3cd56d2f1c896dd6
artginzburg <art.ginzburg@ya.ru>
2021-07-01 13:46:22 +0300

    add createElementsList() to Languages

--- src/components/Languages.tsx
+++ src/components/Languages.tsx
@@ -4,21 +4,43 @@ import MD, { Fragment, LineBreak, Heading } from 'jsx-md';
 
 import OneLineList from './jsx-md/OneLineList';
 
+const MAXIMUM_RATING = 10;
+
+const languages = {
+  speaking: {
+    English: 9,
+    Russian: 10,
+  },
+  coding: {
+    JavaScript: 8,
+    Swift: 6,
+    'Shell script': 6,
+  },
+};
+
+function createElementsList(sector) {
+  const elementsList = [];
+  const object = languages[sector];
+  for (const key in object) {
+    if (Object.prototype.hasOwnProperty.call(object, key)) {
+      const el = object[key];
+      elementsList.push(`${key} <sup>${el}/${MAXIMUM_RATING}</sup>`);
+    }
+  }
+  return elementsList;
+}
+
 function Languages() {
   return (
     <>
       <Heading level={3}>Languages</Heading>
-      <OneLineList>
-        {'English <sup>9/10</sup>'}
-        {'Russian <sup>10/10</sup>'}
-      </OneLineList>
+
+      <OneLineList>{createElementsList('speaking')}</OneLineList>
+
       <LineBreak />
       <LineBreak />
-      <OneLineList>
-        {'JavaScript <sup>8/10</sup>'}
-        {'Swift <sup>6/10</sup>'}
-        {'Shell script <sup>6/10</sup>'}
-      </OneLineList>
+
+      <OneLineList>{createElementsList('coding')}</OneLineList>
     </>
   );
 }
