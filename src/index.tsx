20d2f1e82c894b03293689b8576f4d2169f03c55
artginzburg <art.ginzburg@ya.ru>
2021-07-01 13:26:44 +0300

    remove unused import

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,5 @@
-// We need to tell the JSX transpiler that in this file,
-// instead of React we use the custom createElement and Fragment
-// functions from jsx-md
+// We need to tell the JSX transpiler that in this file, instead of React we use the custom createElement and Fragment functions from jsx-md
 /* @jsx MD */
-/* @jsxFrag Fragment */
 import MD, { render } from 'jsx-md';
 import { writeFileSync } from 'fs';
 
